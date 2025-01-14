999f1a59fed92ae5e4dc642c245305c45ec13941
Matthew Elgert <mdelgert@yahoo.com>
2025-01-14 17:11:49 -0500

    Remove outdated release branch workflow and related scripts

--- temp/files.sh
+++
@@ -1,7 +0,0 @@
-find . -mindepth 1 \
-  -name '.git' -prune -o \
-  -name '.gitignore' -prune -o \
-  -path "./lib/SimpleLibrary" -prune -o \
-  -path "./lib/SimpleLibrary/*" -prune -o \
-  -print
-
