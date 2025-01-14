ac2940bd62e86f96be80e6cde6049bc3d6f905d3
Matthew Elgert <mdelgert@yahoo.com>
2025-01-14 17:08:04 -0500

    Update greeting message in SimpleLibrary to include version number

--- lib/SimpleLibrary/src/SimpleLibrary.cpp
+++ lib/SimpleLibrary/src/SimpleLibrary.cpp
@@ -5,7 +5,7 @@ SimpleLibrary::SimpleLibrary() {
 }
 
 void SimpleLibrary::printHello() {
-    Serial.println("Hello, World!");
+    Serial.println("Hello, World 1.0");
 }
 
 int SimpleLibrary::addNumbers(int a, int b) {
