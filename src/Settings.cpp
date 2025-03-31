a796f4e95178a0a4bc2bd874cbf250f78a49fed0
Matthew Elgert <mdelgert@yahoo.com>
2025-03-30 22:21:53 -0400

    Implement Settings class for device configuration and WiFi management

---
+++ src/Settings.cpp
@@ -0,0 +1,5 @@
+// Settings.cpp
+#include "Settings.h"
+
+Settings Settings::instance;           // Define the static instance
+Settings& settings = Settings::getInstance(); // Define the global reference
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
+#include <Arduino.h>
 #include <SimpleLibrary.h>
+#include "Settings.h"
+#include "OneButton.h"
 
+#define PIN_INPUT 0
+
+OneButton button(PIN_INPUT, true);
 SimpleLibrary lib;
+
+// Action to be performed when the button is pressed
+void onButtonPress() {
+    Serial.println("Button Pressed!");
+}
 
 void setup() {
     Serial.begin(115200);
-    lib.printHello();
+    settings.setDeviceName("MyESP32");
+    settings.setWifiSSID("HomeNetwork");
+    // Attach single click event to the button
+    button.attachClick(onButtonPress);
 }
 
 void loop() {
-    // Do nothing
+    //lib.printHello();
+    //Serial.print("Device Name: ");
+    //Serial.println(settings.getDeviceName());
+    //delay(1000); // Wait for 1 second
+    button.tick();
 }
